cd988fb3d582175ba75b69b74e90eb4e40b5ae74
motokikasai <kmomoky@hotmail.com>
2020-03-04 23:57:35 +0100

    Review and retry to apply tab function with jQuery

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-$(".tab").click(function() {
-  $(".tab.tab-clicked").removeClass("tab-clicked");
+$(".tab").on("click", function() {
+  const tab_id = $(this).attr("data-tab");
+  $(".tab-clicked").removeClass("tab-clicked");
 
   $(this).addClass("tab-clicked");
 
-  // console.log($(this).next());
-
-  // $(this).next().attr(
-  //   "data-tabs"
-  // );
-
-  // if (toggle === "closed") {
-  // $(".content").attr("data-tabs", "closed");
   $(".content").hide();
-  $(this)
-    .next()
-    .fadeIn(1000);
-  // .attr("data-tabs", "open")
-  // .hide()
-  // }
+  $("#" + tab_id).fadeIn(1000);
 });
